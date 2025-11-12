41de70fc1303e3a67c0258435e109244f41de666
alex_zamastil <alex.zamastil@gmail.com>
2025-11-12 21:40:34 +0100

    overview - table and charts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,37 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
+// main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom, APP_INITIALIZER, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeCs from '@angular/common/locales/cs';
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+import { App } from './app/app';
+import { routes } from './app/app-routing-module';
+import { KeycloakService } from './app/keycloak.service';
+import { AuthInterceptor } from './app/AuthInterceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+registerLocaleData(localeCs);
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(FormsModule, ReactiveFormsModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    KeycloakService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (kc: KeycloakService) => () => kc.init(),
+      deps: [KeycloakService],
+      multi: true,
+    },
+    { provide: LOCALE_ID, useValue: 'cs-CZ' },
+  ],
+}).catch(err => console.error(err));
