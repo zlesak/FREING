7f255cd8694b7b0b69e74f99149792fa7a291794
Foglas <kubik.fogl@gmail.com>
2025-11-16 11:09:25 +0100

    minor changes

--- backend/common/src/main/kotlin/com/uhk/fim/prototype/common/exceptions/AbstractResponseException.kt
+++ backend/common/src/main/kotlin/com/uhk/fim/prototype/common/exceptions/AbstractResponseException.kt
@@ -1,8 +1,16 @@
 package com.uhk.fim.prototype.common.exceptions
 
+import com.uhk.fim.prototype.common.handlers.GlobalExceptionHandler
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 
 open class AbstractResponseException(
-    override val message: String,
+    final override val message: String,
     open val httpStatus: HttpStatus,
-) : RuntimeException()
+) : RuntimeException(){
+    private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    init {
+        logger.warn(message)
+    }
+}
